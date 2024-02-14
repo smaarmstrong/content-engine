41c8312c3545c7cd5841e4e0f0bb4f11a0c58a4d
smaa <smaa@localhost.localdomain>
2024-02-13 17:12:45 -0800

    setup environmental variables with ConfigMaps

--- src/smaahome/settings.py
+++ src/smaahome/settings.py
@@ -9,13 +9,13 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+# import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+# print(os.environ.get("WELCOME_MSG"))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mb6vyf7a%wwzv32%u-p*i^&2mm*_zd3i7k$jp#4obg=nj@eenc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True # twingate
 
 ALLOWED_HOSTS = []
 
